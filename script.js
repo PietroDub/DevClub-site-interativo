18629e76110d722dee1251b2db2395ae68483f2d
PietroDub <pietro.henriquevito@gmail.com>
2025-08-11 22:56:17 -0300

    DevClub Base

---
+++ script.js
@@ -0,0 +1,34 @@
+let list = document.querySelectorAll('.item')
+let prev = document.querySelector('#prev')
+let next = document.querySelector('#next')
+
+let count = list.length // 3
+let active = 0
+
+function trade(){
+    console.log("Avançou!")
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    
+    //quando for maior que o valor final, reseta
+    if(active >= count - 1){
+        active = 0
+    } else {
+        active++
+    }
+        list[active].classList.add('active')
+}
+
+function back(){
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    if(active <= 0){
+        active = 2
+    } else {
+        active--
+    }
+
+    list[active].classList.add('active')
+}
